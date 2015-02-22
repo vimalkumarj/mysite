c6a1cf192c954e77a2006883f5aab3211ee32333
vimalkumarj <vimalkumar.j@gmail.com>
2015-02-22 21:26:43 +0530

    Changes to the Site

--- MySite/routes/index.ts
+++ MySite/routes/index.ts
@@ -4,13 +4,13 @@
 import express = require('express');
 
 export function index(req: express.Request, res: express.Response) {
-    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+    res.render('index', { title: 'Vimalkumar J', year: new Date().getFullYear() });
 };
 
 export function about(req: express.Request, res: express.Response) {
-    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page.' });
+    res.render('about', { title: 'About Vimal', year: new Date().getFullYear(), message: 'Your application description page.' });
 };
 
 export function contact(req: express.Request, res: express.Response) {
-    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page.' });
+    res.render('contact', { title: 'Contact Vimal', year: new Date().getFullYear(), message: 'Your contact page.' });
 };
